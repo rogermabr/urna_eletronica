3880c6525f48053ffd9217b19e24caa1eafab656
Roger Albuquerque <roger_alb@ymail.com>
2020-09-17 13:18:27 -0300

    Add files via upload

---
+++ etapas.js
@@ -0,0 +1,58 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero:'11789',
+                nome: 'Fulano de Sá',
+                partido: 'ABC',
+                fotos: [
+                    {url: '11789.jpg', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: '40698',
+                nome: 'Siclano da Silva',
+                partido:'PDV',
+                fotos: [
+                    {url: '40698.jpg', legenda: 'Vereador'}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero:'10',
+                nome: 'Beutrano',
+                partido: 'ABC',
+                vice: 'Creyssin',
+                fotos: [
+                    {url:'10.jpg', legenda: 'Prefeito'},
+                    {url:'10_2.jpg', legenda: 'Vice-Prefeito', small:true}
+                ]
+            },
+            {
+                numero: '15',
+                nome: 'Senhor Aleatório',
+                partido: 'ZYX',
+                vice: 'Peidim',
+                fotos: [
+                    {url:'15.jpg', legenda: 'Prefeito'},
+                    {url:'15_2.jpg', legenda: 'Vice-Prefeito', small:true}
+                ]
+            },
+            ]
+        }
+];
+
+
+
+
+
+
+
+
